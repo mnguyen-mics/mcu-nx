7fb84d1d646753f6567529954cb840dfde0cdbb3
Loïc Girault <lgirault@mediarithmics.com>
2017-11-17 09:49:19 +0100

    Add mcs-services

---
+++ mcs-services/src/MicsApi.ts
@@ -0,0 +1,9 @@
+
+import auth from './AuthenticationStorage';
+import {DefaultApi, Configuration} from 'mcs-public-api/lib';
+
+export * from 'mcs-public-api/lib';
+
+export default new DefaultApi(new Configuration({apiKey: auth.getAccessToken }));
+
+
