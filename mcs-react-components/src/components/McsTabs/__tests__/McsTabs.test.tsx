6a00a6c6334dba71f1cabcff0ef25e975e6d1bc4
Maxime Leuger <mleuger@mediarithmics.com>
2019-01-24 15:52:45 +0100

    MICS-Tribe rm useless import
    remove useless import in test file

--- mcs-react-components/src/components/McsTabs/__tests__/McsTabs.test.tsx
+++ mcs-react-components/src/components/McsTabs/__tests__/McsTabs.test.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { List } from 'antd';
 import { MemoryRouter } from 'react-router';
 import { IntlProvider } from 'react-intl';
 import * as TestRenderer from 'react-test-renderer';
