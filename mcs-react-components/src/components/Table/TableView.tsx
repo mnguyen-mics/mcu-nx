cc9fdefab1b302642235afc6256b5afa62f4bc36
Maxime Leuger <mleuger@mediarithmics.com>
2019-04-02 18:00:25 +0200

    MICS-Tribe add tests for TableView + improve cosmos rendering
    add tests for TableView + improve fixture

--- mcs-react-components/src/components/Table/TableView.tsx
+++ mcs-react-components/src/components/Table/TableView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import cuid from 'cuid';
+import * as cuid from 'cuid';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
 import { Menu, Table, Dropdown } from 'antd';
 import { TableProps, ColumnProps, TableRowSelection } from 'antd/lib/table';
--- mcs-react-components/src/components/Table/__fixtures__/TableView.fixture.tsx
+++ mcs-react-components/src/components/Table/__fixtures__/TableView.fixture.tsx
@@ -1,20 +1,73 @@
 import * as React from 'react';
 import { IntlProvider } from 'react-intl';
-import TableView from '../TableView';
+import TableView, { TableViewProps } from '../TableView';
+import { Divider, Icon } from 'antd';
+import { TableProps } from 'antd/lib/table';
 
-// const props: TableViewProps = {
-//   fetchData: () => Promise.resolve(["ouane","tou","tri"]),
-//   renderItem: (item) => <List.Item>{item}</List.Item>,
-//   storeItemData: () => undefined
-// };
+export type CombinedTableViewProps = TableViewProps<any> & TableProps<any>;
 
-const component = (_props: any) => (
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    width: 150,
+    render: (text: string) => {
+      return <a href="javascript:;">{text}</a>;
+    },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+    width: 70,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    width: 360,
+    render: (text: string, record: any) => (
+      <span>
+        <a href="javascript:;">Action 一 {record.name}</a>
+        <Divider type="vertical" />
+        <a href="javascript:;">Delete</a>
+        <Divider type="vertical" />
+        <a href="javascript:;" className="ant-dropdown-link">
+          More actions <Icon type="down" />
+        </a>
+      </span>
+    ),
+  },
+];
+
+const data = [];
+for (let i = 1; i <= 46; i++) {
+  data.push({
+    key: i.toString(),
+    name: 'John Brown',
+    age: `${i}2`,
+    address: `New York No. ${i} Lake Park`,
+    description: `My name is John Brown, I am ${i}2 years old, living in New York No. ${i} Lake Park.`,
+  });
+}
+
+const props: CombinedTableViewProps = {
+  columns: columns,
+  dataSource: data,
+};
+
+const component = (_props: CombinedTableViewProps) => (
   <IntlProvider locale="en">
-    <TableView dataSource={[]} />
+    <TableView {...props} />
   </IntlProvider>
 );
 
 export default {
   component,
-  props: {},
+  props,
 };
