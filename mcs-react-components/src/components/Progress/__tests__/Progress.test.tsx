88bd0339d26dee97a10d5ba9c9817267bfa7c715
Trestan Mervin <tmervin@mediarithmics.com>
2020-03-03 16:29:38 +0100

    MICS-4014 Progress Component Migration
    Progress component migration

---
+++ mcs-react-components/src/components/Progress/__tests__/Progress.test.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { render } from 'enzyme';
+import Progress, { ProgressProps } from '../Progress';
+
+it('renders the progress', () => {
+  const props: ProgressProps = {
+      percent: 66,
+      label: " % Test",
+  };
+
+  const component = render(
+    <Progress {...props}/>,
+  );
+  expect(component).toMatchSnapshot();
+});
