8fdc6ed49e3988edc4842a43e24651da698c7d5c
hkounka <hkounka@mediarithmics.com>
2020-07-09 16:50:53 +0200

    MICS-5505 : Fixing remarks on last commit
    Fixing remarks on last commit

--- mcs-react-components/src/components/collection-view/CollectionView.tsx
+++ mcs-react-components/src/components/collection-view/CollectionView.tsx
@@ -1,54 +1,52 @@
-import * as React from 'react';
-import { Row, Col, Spin } from 'antd';
-import cuid from 'cuid';
-import Pagination, { PaginationProps } from 'antd/lib/pagination/Pagination';
+import * as React from 'react'
+import { Row, Col, Spin } from 'antd'
+import cuid from 'cuid'
+import Pagination, { PaginationProps } from 'antd/lib/pagination/Pagination'
 
 export interface CollectionViewProps {
-  collectionItems: JSX.Element[];
-  pagination?: PaginationProps;
-  gutter?: number;
-  span?: number;
-  loading?: boolean;
+	collectionItems: JSX.Element[]
+	pagination?: PaginationProps
+	gutter?: number
+	span?: number
+	loading?: boolean
 }
 
 class CollectionView extends React.Component<CollectionViewProps> {
+	static defaultProps: Partial<CollectionViewProps> = {
+		gutter: 20,
+		span: 6,
+		loading: false,
+	}
 
-  static defaultProps: Partial<CollectionViewProps> = {
-    gutter: 20,
-    span: 6,
-    loading: false,
-  };
+	render() {
+		const prefixCls = 'mcs-collection-view'
+		const { collectionItems, gutter, span, pagination, loading } = this.props
 
-  render() {
-      const prefixCls="mcs-collection-view"
-    const {
-      collectionItems,
-      gutter,
-      span,
-      pagination,
-      loading,
-    } = this.props;
-
-    return loading ? (<Row style={{ height: '350px' }}><Row className={`${prefixCls}-loading`}><Spin /></Row></Row>) : (
-      <Row className={prefixCls}>
-        <Row gutter={gutter}>
-          {collectionItems.map(item => {
-            return (
-              <Col key={item.key || cuid()} span={span!}>
-                {item}
-              </Col>
-            );
-          })}
-        </Row>
-        { pagination &&
-          <Row className={`${prefixCls}-text-right`}>
-            <Pagination {...pagination} />
-          </Row>
-        }
-      </Row>
-    );
-  }
-
+		return loading ? (
+			<Row style={{ height: '350px' }}>
+				<Row className={`${prefixCls}-loading`}>
+					<Spin />
+				</Row>
+			</Row>
+		) : (
+			<Row className={prefixCls}>
+				<Row gutter={gutter}>
+					{collectionItems.map((item) => {
+						return (
+							<Col key={item.key || cuid()} span={span!}>
+								{item}
+							</Col>
+						)
+					})}
+				</Row>
+				{pagination && (
+					<Row className={`${prefixCls}-text-right`}>
+						<Pagination {...pagination} />
+					</Row>
+				)}
+			</Row>
+		)
+	}
 }
 
-export default CollectionView;
+export default CollectionView
