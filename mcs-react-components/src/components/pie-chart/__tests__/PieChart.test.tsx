36027a882d1508acfafc1ebe34aa3f82a6e9accb
Trestan Mervin <tmervin@mediarithmics.com>
2020-09-10 17:15:11 +0200

    MICS-FRONT PieChart Migration
    Migration of `PieChart` component

---
+++ mcs-react-components/src/components/pie-chart/__tests__/PieChart.test.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { MemoryRouter } from 'react-router';
+import { IntlProvider } from 'react-intl';
+import { render } from 'enzyme';
+import PieChart, { PieChartProps } from '../PieChart';
+
+it('renders the pie chart', () => {
+  const props: PieChartProps = {
+    options: {
+      chart: {
+        plotShadow: false,
+        type: 'pie',
+      },
+      title: {
+        text: 'Browser market shares in January, 2018',
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+      },
+      accessibility: {},
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          },
+        },
+      },
+      series: [
+        {
+          name: 'Brands',
+          type: 'pie',
+          data: [
+            {
+              name: 'Chrome',
+              y: 61.41,
+              selected: true,
+            },
+            {
+              name: 'Internet Explorer',
+              y: 11.84,
+            },
+            {
+              name: 'Firefox',
+              y: 10.85,
+            },
+            {
+              name: 'Edge',
+              y: 4.67,
+            },
+            {
+              name: 'Safari',
+              y: 4.18,
+            },
+            {
+              name: 'Sogou Explorer',
+              y: 1.64,
+            },
+            {
+              name: 'Opera',
+              y: 1.6,
+            },
+            {
+              name: 'QQ',
+              y: 1.2,
+            },
+            {
+              name: 'Other',
+              y: 2.61,
+            },
+          ],
+        },
+      ],
+    },
+  };
+  
+  const component = render(
+    <IntlProvider locale="en">
+      <MemoryRouter>
+        <PieChart {...props} />
+      </MemoryRouter>
+    </IntlProvider>,
+  );
+  expect(component).toMatchSnapshot();
+});
