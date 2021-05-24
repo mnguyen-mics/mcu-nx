22ea1995b507189a18d047d519c8807ef66e68b5
Oussama Assassi <oassassi@mediarithmics.com>
2021-05-24 16:34:50 +0200

    MICS-DEV import forgot password component from advanced comp
    import forgot password component from advanced component library

--- app/src/containers/main/Main.tsx
+++ app/src/containers/main/Main.tsx
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import { Route, Switch } from 'react-router-dom';
 import * as React from "react";
-import { Login, Store } from '@mediarithmics-private/advanced-component';
+import { Login, ForgotPassword, Store, IocProvider, container } from '@mediarithmics-private/advanced-component';
 import { Provider } from 'react-redux';
 import { HomePage } from '../home';
+
+
 const store = Store();
 class Main extends React.Component {
 
@@ -12,10 +14,19 @@ class Main extends React.Component {
   }
   render() {
     const loginRouteRender = () => {
-      return <Login />
+      return <Login forgotPasswordRoute={'/forgot_password'} />
+    };
+
+    const forgotPasswordRouteRender = () => {
+      return <IocProvider container={container}>
+          <ForgotPassword /> 
+        </IocProvider>
     };
     return <Switch>
       <Route exact={true} path='/' component={HomePage} />
+
+      <Route exact={true} path='/forgot_password' component={forgotPasswordRouteRender} />
+
 
       <Provider store={store}>
         <Route path="/login" render={loginRouteRender} />
