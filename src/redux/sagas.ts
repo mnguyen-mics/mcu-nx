e3c05b056b120216b985668df215a31f168f3f23
Oussama Assassi <oassassi@mediarithmics.com>
2021-06-09 08:46:52 +0200

    MICS-DEV add App reducers and action to redux store
    add App reducers and action to redux store

--- src/redux/sagas.ts
+++ src/redux/sagas.ts
@@ -1,11 +1,13 @@
 import 'regenerator-runtime/runtime';
 import { all } from 'redux-saga/effects';
+import { appSagas } from './App/sagas';
 import { loginSagas } from './Login/sagas';
 import { sessionSagas } from './Session/sagas';
 import { labelsSagas } from './Labels/sagas';
 import { featuresSagas } from './Features/sagas';
 export default function* sagas() {
   yield all([
+    ...appSagas,
     ...sessionSagas,
     ...loginSagas,
     ...labelsSagas,
