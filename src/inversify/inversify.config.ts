4f00709f40bb190b2381dee43f4126ad58bc1f7e
Ousssama Assassi <oassassi@mediarithmics.com>
2021-09-16 09:59:02 +0200

    MICS-9876 migrate QueryService from navigator
    migrate QueryService from navigator

--- src/inversify/inversify.config.ts
+++ src/inversify/inversify.config.ts
@@ -1,20 +1,23 @@
-import { Container } from 'inversify';
-import { TYPES } from '../constants/types'
-import { IAuthService, AuthService } from '../services/AuthService';
-import getDecorators from 'inversify-inject-decorators';
-import { ILabelService, LabelService } from '../services/LabelsService';
-import OrganisationService, { IOrganisationService } from '../services/OrganisationService';
+import { Container } from "inversify";
+import { TYPES } from "../constants/types";
+import { IAuthService, AuthService } from "../services/AuthService";
+import getDecorators from "inversify-inject-decorators";
+import { ILabelService, LabelService } from "../services/LabelsService";
+import OrganisationService, {
+  IOrganisationService,
+} from "../services/OrganisationService";
+import { IQueryService, QueryService } from "../services/QueryService";
 
 export const container = new Container();
 
-container
-  .bind<IAuthService>(TYPES.IAuthService)
-  .to(AuthService);
+container.bind<IAuthService>(TYPES.IAuthService).to(AuthService);
 container.bind<ILabelService>(TYPES.ILabelService).to(LabelService);
 container
   .bind<IOrganisationService>(TYPES.IOrganisationService)
   .to(OrganisationService)
   .inSingletonScope();
+
+container.bind<IQueryService>(TYPES.IQueryService).to(QueryService);
 export const { lazyInject } = getDecorators(container, false);
 
 export default { container };
