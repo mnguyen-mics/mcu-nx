1d1ffef41a17e5332f9bf7dea6ad9651163fee6a
hkounka <hkounka@mediarithmics.com>
2021-09-20 13:36:49 +0200

    MICS-10587 : add the possibility to have custom content on mentiontag
    add the possibility to have custom content on mentiontag

--- mcs-react-components/src/components/mention-tag/__fixtures__/MentionTag.fixture.tsx
+++ mcs-react-components/src/components/mention-tag/__fixtures__/MentionTag.fixture.tsx
@@ -4,6 +4,7 @@ import MentionTag, { MentionTagProps } from '../MentionTag';
 const props: MentionTagProps = {
   mention: 'ALPHA',
   tooltip: 'tooltip',
+  customContent: 'THIS IS A NEW FEATURE',
 };
 
 export default <MentionTag {...props} />;
