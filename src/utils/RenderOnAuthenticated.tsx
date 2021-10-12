42344c40ab9d33abbcc6aa849624274d59b8e2fb
Gauthier Fontaine <gfontaine@mediarithmics.com>
2021-10-12 19:13:14 +0200

    MICS-11021 Enabling Keycloak use
    This commit introduces changes that allow to enable Keycloak or not.
    Those changes will be used in mediarithmics-computing-console in
    order to allow or not the use of Keycloak for Login pages and
    token management.

--- src/utils/RenderOnAuthenticated.tsx
+++ src/utils/RenderOnAuthenticated.tsx
@@ -1,12 +1,101 @@
+import { Error, Loading } from '@mediarithmics-private/mcs-components-library';
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import { errorMessages, MicsReduxState } from '..';
+import { UserProfileResource } from '../models/directory/UserProfileResource';
 import KeycloakService from '../services/KeycloakService';
+import { RouteParams } from './AuthenticatedRoute';
+import { withRouter, RouteComponentProps } from 'react-router';
+import { KeycloakPostLogin } from '../redux/KeycloakPostLogin/actions';
+import { InjectedFeaturesProps, injectFeatures } from '../components/Features';
+import RenderWhenHasAccess from './RenderWhenHasAccess';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
 
-class RenderOnAuthenticated extends React.Component {
+export interface RenderOnAuthenticatedProps {
+  requiredFeatures?: string | string[];
+  requireDatamart?: boolean;
+}
+
+interface MapStateToProps {
+  connectedUser: UserProfileResource;
+  state: MicsReduxState;
+}
+
+interface MapDispatchToProps {
+  keycloakPostLoginAction: () => void;
+}
+
+type Props = RenderOnAuthenticatedProps &
+  MapStateToProps &
+  MapDispatchToProps &
+  InjectedFeaturesProps &
+  InjectedIntlProps &
+  RouteComponentProps<RouteParams>;
+
+class RenderOnAuthenticated extends React.Component<Props> {
+  static defaultProps = {
+    requiredFeatures: undefined,
+    requireDatamart: false,
+  };
+
+  componentDidMount() {
+    const { connectedUser, keycloakPostLoginAction } = this.props;
+    if (
+      KeycloakService.isKeycloakEnabled() &&
+      KeycloakService.isLoggedIn() &&
+      (!connectedUser || !connectedUser.id)
+    ) {
+      keycloakPostLoginAction();
+    }
+  }
+
   render() {
-    const { children } = this.props;
+    const {
+      children,
+      state,
+      requiredFeatures,
+      requireDatamart,
+      intl: { formatMessage },
+    } = this.props;
 
-    return KeycloakService.isLoggedIn() ? children : null;
+    const defaultLoading = <Loading isFullScreen={true} />;
+    const defaultError = <Error message={formatMessage(errorMessages.generic)} />;
+
+    if (!KeycloakService.isKeycloakEnabled() || !KeycloakService.isLoggedIn()) {
+      return defaultError;
+    }
+
+    // If KeycloakPostLogin not already finished
+    if (!state.keycloakPostLogin.done) {
+      return defaultLoading;
+    }
+
+    // If KeycloakPostLogin done and failed
+    if (state.keycloakPostLogin.hasFailed) {
+      return defaultError;
+    }
+
+    return (
+      <RenderWhenHasAccess requiredFeatures={requiredFeatures} requireDatamart={requireDatamart}>
+        {children}
+      </RenderWhenHasAccess>
+    );
   }
 }
 
-export default RenderOnAuthenticated;
+const mapStateToProps = (state: MicsReduxState) => ({
+  connectedUser: state.session.connectedUser,
+  state: state,
+});
+
+const mapDispatchToProps = {
+  keycloakPostLoginAction: KeycloakPostLogin.keycloakPostLogin,
+};
+
+export default compose<Props, RenderOnAuthenticatedProps>(
+  withRouter,
+  injectIntl,
+  injectFeatures,
+  connect(mapStateToProps, mapDispatchToProps),
+)(RenderOnAuthenticated);
