9b28e62d2e85d08f13391bcea129424bc692f2ea
Ousssama Assassi <oassassi@mediarithmics.com>
2021-10-15 17:15:22 +0200

    MICS-FIX fix tsconfig issues
    fix typscript config issues

--- src/redux/middleware/logoutListener.ts
+++ src/redux/middleware/logoutListener.ts
@@ -1,6 +1,7 @@
 import LocalStorage from '../../services/LocalStorage';
 
 // TODO test to do this local-storage operation in saga next to LOGOUT listener
+// @ts-ignore
 export default store => next => action => {
   // eslint-disable-line no-unused-vars
 
