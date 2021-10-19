2230769820534028bbc20a8df16080d4486be931
Maxime Leuger <mleuger@mediarithmics.com>
2021-10-19 17:10:32 +0200

    MICS-10973 Batch Definition Plugin List
    this commit implements batch definition plugin list

--- app/src/services/PluginService.ts
+++ app/src/services/PluginService.ts
@@ -1,11 +1,7 @@
 import { injectable } from 'inversify';
 import { PluginResource, PluginType, PluginVersionResource } from '../models/plugin/plugins';
 import { PaginatedApiParam } from '../utils/ApiHelper';
-import { ApiService } from '@mediarithmics-private/advanced-components';
-import {
-  DataListResponse,
-  DataResponse,
-} from '@mediarithmics-private/advanced-components/lib/services/ApiService';
+import ApiService, { DataListResponse, DataResponse } from './ApiService';
 
 interface GetPluginOptions extends Omit<PaginatedApiParam, 'first_result'> {
   plugin_type?: PluginType;
