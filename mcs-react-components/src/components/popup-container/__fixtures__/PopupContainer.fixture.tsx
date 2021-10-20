2d447121155a7dc1f711070449c694ef3bc3bdea
Nicolas Dussart <ndussart@mediarithmics.com>
2021-10-20 11:01:22 +0200

    MICS-5821 PopupContainer fixtures/tests
    Add fixtures and tests for PopupContainer components

---
+++ mcs-react-components/src/components/popup-container/__fixtures__/PopupContainer.fixture.tsx
@@ -0,0 +1,65 @@
+import { DownOutlined } from '@ant-design/icons';
+import { Button, Menu, Select as AntdSelect, Divider } from 'antd';
+import * as React from 'react';
+import { DatePicker, Dropdown, Popover, Select } from '../PopupContainer';
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target='_blank' rel='noopener noreferrer' href='https://www.mediarithmics.com'>
+        1st menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item icon={<DownOutlined />} disabled={true}>
+      <a target='_blank' rel='noopener noreferrer' href='https://www.mediarithmics.com'>
+        2nd menu item (disabled)
+      </a>
+    </Menu.Item>
+    <Menu.Item disabled={true}>
+      <a target='_blank' rel='noopener noreferrer' href='https://www.mediarithmics.com'>
+        3rd menu item (disabled)
+      </a>
+    </Menu.Item>
+    <Menu.Item danger={true}>a danger item</Menu.Item>
+  </Menu>
+);
+
+const content = (
+  <div>
+    <p>Content</p>
+    <p>Content</p>
+  </div>
+);
+
+const { Option } = AntdSelect;
+
+const preventDefault = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => e.preventDefault();
+
+export default (
+  <div>
+    <h1>DatePicker</h1>
+    <DatePicker />
+    <Divider />
+    <h1>Dropdown</h1>
+    <Dropdown overlay={menu}>
+      <a className='ant-dropdown-link' onClick={preventDefault}>
+        Hover me <DownOutlined />
+      </a>
+    </Dropdown>
+    <Divider />
+    <h1>Popover</h1>
+    <Popover content={content} title='Title'>
+      <Button type='primary'>Hover me</Button>
+    </Popover>
+    <Divider />
+    <h1>Select</h1>
+    <Select defaultValue='lucy' style={{ width: 120 }}>
+      <Option value='jack'>Jack</Option>
+      <Option value='lucy'>Lucy</Option>
+      <Option value='disabled' disabled={true}>
+        Disabled
+      </Option>
+      <Option value='Yiminghe'>yiminghe</Option>
+    </Select>
+  </div>
+);
