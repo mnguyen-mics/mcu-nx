5df13a51fa99733b6d320331a29d0bcc3cf0cc54
Nicolas Guillomon <nguillomon@mediarithmics.com>
2021-12-06 10:28:31 +0100

    MICS-11434 Fix dependencies alerts
    Remove react-dev-utils as there is a critical and is not used
    Remove react-refresh-webpack-plugin
    Update react-syntax-highlighter to ^15.4.5

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,5 +1,5 @@
 declare module '@react-mock/fetch';
 declare module '@react-mock/localstorage';
 declare module 'react-syntax-highlighter';
-declare module 'react-syntax-highlighter/styles/hljs';
+declare module 'react-syntax-highlighter/dist/esm/styles/hljs';
 declare module 'react-copy-to-clipboard';
