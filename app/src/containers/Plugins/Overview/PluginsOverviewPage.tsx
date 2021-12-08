f0f2dfdd9b164d8923bf7b1ecbb70c9d9a8be542
m-livney <mlivney@mediarithmics.com>
2021-12-08 14:00:15 +0100

    MICS-11494 Add dashboards in plugins overview
    Add dashboards in plugins overview.

--- app/src/containers/Plugins/Overview/PluginsOverviewPage.tsx
+++ app/src/containers/Plugins/Overview/PluginsOverviewPage.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import {
+  DashboardLayout,
   withDatamartSelector,
   WithDatamartSelectorProps,
 } from '@mediarithmics-private/advanced-components';
@@ -9,6 +10,10 @@ import { Content } from 'antd/lib/layout/layout';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
 import messages from '../messages';
 import { Actionbar } from '@mediarithmics-private/mcs-components-library';
+import {
+  ChartType,
+  SourceType,
+} from '@mediarithmics-private/advanced-components/lib/services/ChartDatasetService';
 type Props = RouteComponentProps<{ organisationId: string }> &
   WithDatamartSelectorProps &
   InjectedIntlProps;
@@ -17,17 +22,50 @@ class PluginsOverviewPage extends React.Component<Props> {
   render() {
     const {
       intl: { formatMessage },
+      selectedDatamartId,
     } = this.props;
 
     const breadcrumbPaths = [
-      <span>{formatMessage(messages.plugins)}</span>,
-      <span>{formatMessage(messages.pluginsOverview)}</span>,
+      <span key={messages.plugins.id}>{formatMessage(messages.plugins)}</span>,
+      <span key={messages.pluginsOverview.id}>{formatMessage(messages.pluginsOverview)}</span>,
     ];
+
+    const chartType: ChartType = 'metric';
+    const sourceType: SourceType = 'otql';
+
+    const schema = {
+      sections: [
+        {
+          title: '',
+          cards: [
+            {
+              x: 0,
+              y: 0,
+              w: 4,
+              h: 1,
+              charts: [
+                {
+                  title: 'Total user points',
+                  type: chartType,
+                  dataset: {
+                    type: sourceType,
+                    query_text: 'SELECT @count{} FROM UserPoint',
+                    datamart_id: selectedDatamartId,
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
 
     return (
       <div className='ant-layout'>
         <Actionbar pathItems={breadcrumbPaths} />
-        <Content className='mcs-content-container'></Content>
+        <Content className='mcs-content-container'>
+          <DashboardLayout datamart_id={selectedDatamartId} schema={schema} />
+        </Content>
       </div>
     );
   }
