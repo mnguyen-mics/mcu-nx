9f960361f8ac83e80871436d16feb6a7ea7b2e47
Maxime Leuger <mleuger@mediarithmics.com>
2021-12-14 16:32:17 +0100

    MICS-11460-bis Update IntegrationBatchService
    this commit updates integration batch service and plugin instance service

--- src/services/IntegrationBatchService.ts
+++ src/services/IntegrationBatchService.ts
@@ -1,13 +1,9 @@
 import { injectable } from 'inversify';
 import { CronStatus, IntegrationBatchResource } from '../models/plugin/Plugins';
 import PluginInstanceService from './PluginInstanceService';
-import ApiService, { DataListResponse, DataResponse, StatusCode } from './ApiService';
+import { DataListResponse, DataResponse, StatusCode } from './ApiService';
 import { PaginatedApiParam } from '../utils/ApiHelper';
-import {
-  // ExternalModelName,
-  // JobExecutionPublicStatus,
-  PublicJobExecutionResource,
-} from '../models/job/jobs';
+import { PublicJobExecutionResource } from '../models/job/jobs';
 import { PluginLayout } from '../models/plugin/PluginLayout';
 
 export interface IntegrationBatchInstanceOptions extends PaginatedApiParam {
@@ -23,11 +19,19 @@ export interface IIntegrationBatchService extends PluginInstanceService<Integrat
     organisationId: string,
     options: IntegrationBatchInstanceOptions,
   ) => Promise<DataListResponse<IntegrationBatchResource>>;
+  updateIntegrationBatchInstance: (
+    id: string,
+    instanceResource: Partial<IntegrationBatchResource>,
+  ) => Promise<DataResponse<IntegrationBatchResource>>;
   getAllInstanceFilterProperties: () => Promise<DataListResponse<string>>;
   getBatchInstanceExecutions: (
     batchInstanceId: string,
     options: IntegrationBatchInstanceOptions,
   ) => Promise<DataListResponse<PublicJobExecutionResource>>;
+  createIntegrationBatchInstanceExecution: (
+    instanceId: string,
+    executionResource: Partial<PublicJobExecutionResource>,
+  ) => Promise<DataResponse<PublicJobExecutionResource>>;
   getBatchInstanceExecutionsForOrganisation: (
     organisationId: string,
     options: PaginatedApiParam,
@@ -56,8 +60,14 @@ export default class IntegrationBatchService
   }
 
   getIntegrationBatchInstance(id: string): Promise<DataResponse<IntegrationBatchResource>> {
-    const endpoint = `integration_batch_instances/${id}`;
-    return ApiService.getRequest(endpoint);
+    return this.getInstanceById(id);
+  }
+
+  updateIntegrationBatchInstance(
+    id: string,
+    instanceResource: Partial<IntegrationBatchResource>,
+  ): Promise<DataResponse<IntegrationBatchResource>> {
+    return this.updatePluginInstance(id, instanceResource);
   }
 
   getBatchInstanceExecutions(
@@ -68,56 +78,15 @@ export default class IntegrationBatchService
       count: 0,
       data: [],
     });
+    // TODO: replace with :
+    // return this.getInstanceExecutions(integrationBatchId);
+  }
 
-    // const endpoint = `integration_batch_instances/${integrationBatchId}/executions`;
-    // return ApiService.getRequest(endpoint);
-
-    // Route does not exist in backend
-    // Mock to test:
-    // return Promise.resolve({
-    //   status: 'ok' as StatusCode,
-    //   count: 3,
-    //   data: [
-    //     {
-    //       id: '1',
-    //       status: 'SUCCEEDED' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       start_date: 1637085198120,
-    //       duration: 176500000,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //     {
-    //       id: '2',
-    //       status: 'FAILED' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       start_date: 1637091181000,
-    //       duration: 9999,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //     {
-    //       id: '3',
-    //       status: 'CANCELED' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       start_date: 1637086188452,
-    //       duration: 123,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //     {
-    //       id: '4',
-    //       status: 'PENDING' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //     {
-    //       id: '5',
-    //       status: 'RUNNING' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       start_date: 1637087188452,
-    //       duration: 107613,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //   ],
-    // });
+  createIntegrationBatchInstanceExecution(
+    instanceId: string,
+    executionResource: Partial<PublicJobExecutionResource>,
+  ): Promise<DataResponse<PublicJobExecutionResource>> {
+    return this.createInstanceExecution(instanceId, executionResource);
   }
 
   getBatchInstanceExecutionsForOrganisation(
@@ -129,61 +98,6 @@ export default class IntegrationBatchService
       count: 0,
       data: [],
     });
-
-    // const params = {
-    //   organisation_id : organisationId,
-    //   ...options,
-    // };
-
-    // const endpoint = `integration_batch_instances.executions`;
-    // return ApiService.getRequest(endpoint, params);
-
-    // Route does not exist in backend
-    // Mock to test:
-    // return Promise.resolve({
-    //   status: 'ok' as StatusCode,
-    //   count: 5,
-    //   data: [
-    //     {
-    //       id: '1',
-    //       status: 'SUCCEEDED' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       start_date: 1637085198120,
-    //       duration: 176500000,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //     {
-    //       id: '2',
-    //       status: 'FAILED' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       start_date: 1637091181000,
-    //       duration: 9999,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //     {
-    //       id: '3',
-    //       status: 'CANCELED' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       start_date: 1637086188452,
-    //       duration: 123,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //     {
-    //       id: '4',
-    //       status: 'PENDING' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //     {
-    //       id: '5',
-    //       status: 'RUNNING' as JobExecutionPublicStatus,
-    //       creation_date: 0,
-    //       start_date: 1637087188452,
-    //       duration: 107613,
-    //       external_model_name: 'PUBLIC_DATAMART' as ExternalModelName,
-    //     },
-    //   ],
-    // });
   }
 
   getAllInstanceFilterProperties() {
