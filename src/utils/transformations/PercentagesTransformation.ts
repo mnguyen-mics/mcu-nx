6cc43f034784b91d3ac6702876315758a27b2586
Igor Epimakhov <iepimakhov@mediarithmics.com>
2022-07-26 11:25:03 +0200

    MICS-13744 Fix to-percentage dashboard transformation
    * implemented percentage count for multiple series in dataset
    
    Signed-off-by: Igor Epimakhov <iepimakhov@mediarithmics.com>

--- src/utils/transformations/PercentagesTransformation.ts
+++ src/utils/transformations/PercentagesTransformation.ts
@@ -1,8 +1,19 @@
 import { AggregateDataset } from '../../models/dashboards/dataset/dataset_tree';
+import { Datapoint } from '../../../node_modules/@mediarithmics-private/mcs-components-library/lib/components/charts/utils.d';
 
 export function percentages(xKey: string, dataset: AggregateDataset): AggregateDataset {
-  const datasetTitle = dataset.metadata.seriesTitles[0];
+  return uniteSeries(
+    dataset.metadata.seriesTitles.map(datasetTitle => {
+      return percentagesBySerie(xKey, dataset, datasetTitle);
+    }),
+  );
+}
 
+export function percentagesBySerie(
+  xKey: string,
+  dataset: AggregateDataset,
+  datasetTitle: string,
+): AggregateDataset {
   const sanitizedDataset = dataset.dataset.filter(line => typeof line[datasetTitle] === 'number');
   const datasetTotalValue = sanitizedDataset
     .map(line => {
@@ -27,4 +38,45 @@ export function percentages(xKey: string, dataset: AggregateDataset): AggregateD
     dataset: percentageDataset,
     type: 'aggregate',
   };
+}
+
+export function uniteSeries(datasets: AggregateDataset[]): AggregateDataset {
+  if (datasets.length > 0) {
+    const keys = datasets[0].dataset.map(line => line.key);
+
+    const resultDataset = keys.map(key => {
+      return uniteLines(
+        datasets
+          .map(dataset => {
+            return dataset.dataset.find(line => line.key === key);
+          })
+          .filter(line => line !== undefined) as Datapoint[],
+      );
+    });
+
+    return {
+      metadata: {
+        seriesTitles: datasets.map(dataset => dataset.metadata.seriesTitles[0]),
+      },
+      type: datasets[0].type,
+      dataset: resultDataset,
+    } as AggregateDataset;
+  } else {
+    return {
+      metadata: {
+        seriesTitles: [],
+      },
+      dataset: [],
+      type: 'aggregate',
+    } as AggregateDataset;
+  }
+}
+
+export function uniteLines(datapoints: Datapoint[]): Datapoint {
+  return datapoints.reduce((previousValue, currentValue) => {
+    return {
+      ...previousValue,
+      ...currentValue,
+    };
+  }, {});
 }
