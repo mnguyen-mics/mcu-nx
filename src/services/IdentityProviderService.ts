1a6cb429fe847f7bc1012c76b4f6e634a4761c30
Nicolas Guillomon <nguillomon@mediarithmics.com>
2022-08-18 15:27:45 +0200

    MICS-13950 Update IdentityProviderService
    Update with new APIs

--- src/services/IdentityProviderService.ts
+++ src/services/IdentityProviderService.ts
@@ -1,9 +1,25 @@
 import { IdentityProviderResource } from './../models/identityProvider/IdentityProviderResource';
-import ApiService, { DataListResponse, DataResponse } from './ApiService';
+import ApiService, { ApiResponse, DataListResponse, DataResponse } from './ApiService';
 import { injectable } from 'inversify';
 import { OrganisationResource } from '../models/organisation/organisation';
 
 export interface IIdentityProviderService {
+  createIdentityProvider: (
+    communityId: string,
+    identityProvider: Pick<
+      IdentityProviderResource,
+      'name' | 'description' | 'provider_type' | 'metadata_xml_url'
+    >,
+  ) => Promise<DataResponse<IdentityProviderResource>>;
+
+  updateIdentityProvider: (
+    communityId: string,
+    identityProviderId: string,
+    identityProvider: Pick<IdentityProviderResource, 'description' | 'metadata_xml_url'>,
+  ) => Promise<DataResponse<IdentityProviderResource>>;
+
+  deleteIdentityProvider: (communityId: string, identityProviderId: string) => Promise<ApiResponse>;
+
   getCommunityIdentityProviders: (
     communityId: string,
   ) => Promise<DataListResponse<IdentityProviderResource>>;
@@ -13,18 +29,53 @@ export interface IIdentityProviderService {
     identityProviderId: string,
   ) => Promise<DataResponse<IdentityProviderResource>>;
 
-  getIdentityProviderAssociatedToOrganisation: (
-    organisationId: string,
-  ) => Promise<DataResponse<IdentityProviderResource>>;
-
   getOrganisationsAssociatedToIdentityProvider: (
     communityId: string,
     identityProviderId: string,
   ) => Promise<DataListResponse<OrganisationResource>>;
+
+  getIdentityProviderAssociatedToOrganisation: (
+    organisationId: string,
+  ) => Promise<DataResponse<IdentityProviderResource>>;
+
+  associateIdentityProviderToOrganisation: (
+    organisationId: string,
+    identityProviderId: string,
+  ) => Promise<DataResponse<OrganisationResource>>;
+
+  dissociateIdentityProviderFromOrganisation: (
+    organisationId: string,
+    identityProviderId: string,
+  ) => Promise<DataResponse<OrganisationResource>>;
 }
 
 @injectable()
 export class IdentityProviderService implements IIdentityProviderService {
+  createIdentityProvider(
+    communityId: string,
+    identityProvider: Pick<
+      IdentityProviderResource,
+      'name' | 'description' | 'provider_type' | 'metadata_xml_url'
+    >,
+  ): Promise<DataResponse<IdentityProviderResource>> {
+    const endpoint = `community/${communityId}/identity_providers`;
+    return ApiService.postRequest(endpoint, identityProvider);
+  }
+
+  updateIdentityProvider(
+    communityId: string,
+    identityProviderId: string,
+    identityProvider: Pick<IdentityProviderResource, 'description' | 'metadata_xml_url'>,
+  ): Promise<DataResponse<IdentityProviderResource>> {
+    const endpoint = `community/${communityId}/identity_providers/${identityProviderId}`;
+    return ApiService.putRequest(endpoint, identityProvider);
+  }
+
+  deleteIdentityProvider(communityId: string, identityProviderId: string): Promise<ApiResponse> {
+    const endpoint = `community/${communityId}/identity_providers/${identityProviderId}`;
+    return ApiService.deleteRequest(endpoint);
+  }
+
   getCommunityIdentityProviders(
     communityId: string,
   ): Promise<DataListResponse<IdentityProviderResource>> {
@@ -40,6 +91,14 @@ export class IdentityProviderService implements IIdentityProviderService {
     return ApiService.getRequest(endpoint);
   }
 
+  getOrganisationsAssociatedToIdentityProvider(
+    communityId: string,
+    identityProviderId: string,
+  ): Promise<DataListResponse<OrganisationResource>> {
+    const endpoint = `community/${communityId}/identity_providers/${identityProviderId}/organisations`;
+    return ApiService.getRequest(endpoint);
+  }
+
   getIdentityProviderAssociatedToOrganisation(
     organisationId: string,
   ): Promise<DataResponse<IdentityProviderResource>> {
@@ -47,11 +106,19 @@ export class IdentityProviderService implements IIdentityProviderService {
     return ApiService.getRequest(endpoint);
   }
 
-  getOrganisationsAssociatedToIdentityProvider(
-    communityId: string,
+  associateIdentityProviderToOrganisation(
+    organisationId: string,
     identityProviderId: string,
-  ): Promise<DataListResponse<OrganisationResource>> {
-    const endpoint = `community/${communityId}/identity_providers/${identityProviderId}/organisations`;
-    return ApiService.getRequest(endpoint);
+  ): Promise<DataResponse<OrganisationResource>> {
+    const endpoint = `organisations/${organisationId}/identity_providers/${identityProviderId}`;
+    return ApiService.postRequest(endpoint, {});
+  }
+
+  dissociateIdentityProviderFromOrganisation(
+    organisationId: string,
+    identityProviderId: string,
+  ): Promise<DataResponse<OrganisationResource>> {
+    const endpoint = `community/${organisationId}/identity_providers/${identityProviderId}`;
+    return ApiService.deleteRequest(endpoint);
   }
 }
