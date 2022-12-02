cb9053988344bb8d353d3a9fdee259b5c3284ee9
Maxime Nguyen <mnguyen.ext@mediarithmics.com>
2022-12-02 11:36:56 +0100

    Fix webpack.prod

--- apps/navigator/webpack.prod.js
+++ apps/navigator/webpack.prod.js
@@ -1,15 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const nrwlConfig = require('@nrwl/react/plugins/webpack.js');
-const VersionPlugin = require('apps/navigator/VersionPlugin');
+const VersionPlugin = require('./VersionPlugin');
 const path = require('path');
-const paths = require('apps/navigator/paths');
+const paths = require('./paths');
 
 module.exports = (config, context) => {
   nrwlConfig(config);
   return merge(config, common, {
-    module: {
-      plugins: [new VersionPlugin({ path: path.resolve(paths.appPath) })],
-    },
+    plugins: [new VersionPlugin({ path: path.resolve(paths.appPath) })],
   });
 };
