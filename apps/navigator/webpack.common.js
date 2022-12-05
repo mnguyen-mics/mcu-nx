c217f9d3fd45f88ea795e9416b001fe0f292bc1b
Maxime Nguyen <mnguyen.ext@mediarithmics.com>
2022-12-05 13:18:34 +0100

    Remove SWC, as advanced-components do not use it

--- apps/navigator/webpack.common.js
+++ apps/navigator/webpack.common.js
@@ -1,20 +1,6 @@
 const webpack = require('webpack');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.[tj]sx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'swc-loader',
-          options: {
-            parseMap: true,
-          },
-        },
-      },
-    ],
-  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.API_ENV': JSON.stringify(process.env.API_ENV),
