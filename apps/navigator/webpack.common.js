4a5c2cda135ff731a5a1e93439e6bf9c3627f04c
Maxime Nguyen <mnguyen.ext@mediarithmics.com>
2022-12-05 16:20:01 +0100

    Disable webpack import warnings

--- apps/navigator/webpack.common.js
+++ apps/navigator/webpack.common.js
@@ -1,6 +1,14 @@
 const webpack = require('webpack');
 
 module.exports = {
+  module: {
+    parser: {
+      javascript: {
+        reexportExportsPresence: false,
+        importExportsPresence: false
+      },
+    },
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.API_ENV': JSON.stringify(process.env.API_ENV),
