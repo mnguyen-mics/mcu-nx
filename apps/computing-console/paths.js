6b908b4bb4bddfc8fbb3cc82a0799c82140c4539
Maxime Nguyen <mnguyen.ext@mediarithmics.com>
2022-12-08 16:43:34 +0100

    Fix VersionPlugin paths on computing-console

--- apps/computing-console/paths.js
+++ apps/computing-console/paths.js
@@ -5,13 +5,13 @@ function resolve(relativePath) {
 }
 
 module.exports = {
-  appHtml: resolve('app/index.html'),
-  appDistHtml: resolve('dist/index.html'),
-  reactAppSrc: resolve('app/src'),
-  appStyleLess: resolve('app/src/styles/index.less'),
+  appHtml: resolve('apps/computing-console/index.html'),
+  appDistHtml: resolve('dist/apps/computing-console/index.html'),
+  reactAppSrc: resolve('apps/computing-console/src'),
+  appStyleLess: resolve('apps/computing-console/src/styles/index.less'),
   appNodeModules: resolve('node_modules'),
-  appPath: resolve('app'),
-  appDistPath: resolve('dist/'),
+  appPath: resolve('apps/computing-console'),
+  appDistPath: resolve('dist/apps/computing-console/'),
   publicPath: '/',
-  publicDistPath: '/',
+  publicDistPath: '/react',
 };
