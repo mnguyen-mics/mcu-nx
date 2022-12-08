fbf2ba86f3344f7e3f95e7b163ec5a4296249eea
Maxime Nguyen <mnguyen.ext@mediarithmics.com>
2022-12-08 17:30:26 +0100

    Merge config to basic lib

--- libs/basic/jest.config.ts
+++ libs/basic/jest.config.ts
@@ -1,11 +1,13 @@
-/* eslint-disable */
 export default {
+  // Setup jest typescript
   displayName: 'basic',
   preset: '../../jest.preset.js',
+  roots: ['<rootDir>/src'],
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: ['./src/utils/Mocks.ts', '<rootDir>/setupEnzyme.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   coverageDirectory: '../../coverage/libs/basic',
 };
